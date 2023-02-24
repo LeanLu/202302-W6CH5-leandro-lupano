85df1a0e4ce23b845a10ab09f37d76ea3c1431ea
Leandro <leandrolupano@hotmail.com>
2023-02-24 19:22:06 +0100

    🏗️ start create method

--- src/repository/knowledges.file.repo.ts
+++ src/repository/knowledges.file.repo.ts
@@ -8,4 +8,6 @@ export class KnowledgesFileRepo {
       .readFile(file, { encoding: 'utf-8' })
       .then((data) => JSON.parse(data) as { [key: string]: string | number }[]);
   }
+
+  create() {}
 }
