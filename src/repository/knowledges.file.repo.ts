441d275942d5d8b966771e39e51080b3c4bb1083
Leandro <leandrolupano@hotmail.com>
2023-02-25 22:37:37 +0100

    ✅ Code improvement

--- src/repository/knowledges.file.repo.ts
+++ src/repository/knowledges.file.repo.ts
@@ -21,24 +21,22 @@ export class KnowledgesFileRepo implements KnowledgesRepoStructure {
     return fs
       .readFile(file, { encoding: 'utf-8' })
       .then((data) => JSON.parse(data) as KnowledgeStructure[]);
+
+    // EJEMPLO SI FUESE ASYNC-AWAIT:
+    // const stringInitialData = await fs.readFile(file, {
+    //   encoding: 'utf-8',
+    // });
+    // const data: KnowledgeStructure[] = JSON.parse(stringInitialData);
   }
 
   async read(id: KnowledgeStructure['id']) {
-    const stringInitialData = await fs.readFile(file, {
-      encoding: 'utf-8',
-    });
-
-    const data: KnowledgeStructure[] = JSON.parse(stringInitialData);
+    const data: KnowledgeStructure[] = await this.readAll();
 
     return data.filter((item) => item.id === id)[0];
   }
 
   async create(knowledge: KnowledgeStructure) {
-    const stringInitialData = await fs.readFile(file, {
-      encoding: 'utf-8',
-    });
-
-    const data: KnowledgeStructure[] = JSON.parse(stringInitialData);
+    const data: KnowledgeStructure[] = await this.readAll();
 
     const newId: number =
       data.length > 0 ? Math.max(...data.map((item) => item.id)) : 0;
@@ -53,11 +51,7 @@ export class KnowledgesFileRepo implements KnowledgesRepoStructure {
   }
 
   async update(knowledge: KnowledgeStructure) {
-    const stringInitialData = await fs.readFile(file, {
-      encoding: 'utf-8',
-    });
-
-    const data: KnowledgeStructure[] = JSON.parse(stringInitialData);
+    const data: KnowledgeStructure[] = await this.readAll();
 
     const dataEdited = data.map((item) =>
       item.id === knowledge.id ? knowledge : item
@@ -71,11 +65,7 @@ export class KnowledgesFileRepo implements KnowledgesRepoStructure {
   }
 
   async delete(id: KnowledgeStructure['id']) {
-    const stringInitialData = await fs.readFile(file, {
-      encoding: 'utf-8',
-    });
-
-    const data: KnowledgeStructure[] = JSON.parse(stringInitialData);
+    const data: KnowledgeStructure[] = await this.readAll();
 
     const dataFiltered = data.filter((item) => item.id !== id);
 
